114461c6d17d5379bc0c264774922711a4af711d
peiyu <42792573+peiyu9806@users.noreply.github.com>
2018-10-18 18:36:36 -0700

    Update

--- main.cpp
+++ main.cpp
@@ -1,16 +1,50 @@
 /**
-
-Description:
-Filename:
-Author:
-Date:
-
+Description: The program will prompt the user to enter a string and a character and print the numbers of the character has been used in the string.
+Filename: main.cpp
+Author: Pei-Yu Kuo
+Date: 10-18-18
 */
+
+#include <iostream>
+#include <string>
 
 using namespace std;
 
 
-int main()
+string get_string(string prompt)
 {
-   return 0;
+    string s;
+    cout << prompt;
+    getline(cin,s);
+    return s;
+
+}
+
+
+int char_count(string s, char c)
+{
+    int count = 0;
+    for(int i = 0; i < s.length(); i++)
+    {
+        char ch = s.at(i);
+
+        if(ch == c)
+        {
+            count ++;
+        }
+    }
+    return count;
+}
+
+
+int main ()
+{   
+    char character;
+    string g_strings = get_string("enter something");
+    cout << "enter the character ";
+    cin >> character;
+
+    cout << "The number is " << char_count(g_strings, character) << endl;
+
+    return 0;
 }
